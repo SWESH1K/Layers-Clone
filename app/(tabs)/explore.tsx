9ac545f54a3101c305a39a2c51f8b182311cd8bb
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-12-16 14:34:21 +0530

    Bottom-sheet added

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import DownloadPicture from '@/components/BottomSheetComponent';
 
 const explore = () => {
+  const [pictureOpen, setPictureOpen] = useState(false);
+
   return (
-    <View style={{flex: 1, alignItems: 'center'}}>
-        <Text>Explore Page</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+          <Text style={{alignSelf: 'center'}}>Explore Page</Text>
+          <Button title='Open Bottom Sheet' onPress={() => {
+            setPictureOpen(true);
+          }}></Button>
+          { pictureOpen && <DownloadPicture onClose={() => {
+            setPictureOpen(false);
+          }}/> }
+      </View>
+    </SafeAreaView>
   )
 }
 
